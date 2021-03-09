d1c4fbfe7c9838029b73c7e045cf28b91cd49b31
Bandanarama <kvoon0@frostburg.edu>
2021-03-08 22:34:29 -0700

    added parsing rows for data for analysis

--- main.py
+++ main.py
@@ -17,4 +17,28 @@ for company in companies:
     news_tables[company] = news_table
     break
 
-a
+parsed_data = []
+
+for company, news_table in news_tables.items():
+    for row in news_table.findAll('tr'):
+
+        title = row.a.get_text
+        date_data = row.td.text.split(' ')
+
+        if len(date_data) == 1:
+            time = date_data[0]
+        else:
+            date = date_data[0]
+            time = date_data[1]
+
+        parsed_data.append([company, date, time, title])
+        
+print(parsed_data)
+
+# amazon_data = news_tables['AMZN']
+# amazon_rows = amazon_data.findAll('tr')
+
+# for index, row in enumerate(amazon_rows):
+#     title = row.a.text
+#     timestamp = row.td.text
+#     print(timestamp + " " + title)
